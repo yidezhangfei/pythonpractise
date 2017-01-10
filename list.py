d9d6ce0a5b3d15f7bcc22c7587db418b46558307
李军 <lijunSI0413@sogou-inc.com>
2017-01-10 22:59:33 +0800

    list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+list = [
+        ['Apple', 'Google', 'Microsoft'],
+        ['Java', 'Python', 'Ruby', 'PhP'],
+        ['Adam', 'Bart', 'Lisa']]
+
+print(list[0][0])
+
+print(list[1][1])
+
+print(list[2][2])
