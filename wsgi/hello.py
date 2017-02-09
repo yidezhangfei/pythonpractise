197fe525c797526894fb4a0f319fa1a99cdb97a3
李军 <lijunSI0413@sogou-inc.com>
2017-02-09 23:46:37 +0800

    modified hello.py

--- wsgi/hello.py
+++ wsgi/hello.py
@@ -2,4 +2,5 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/xml')])
-    return [b'<h1>Hello, Web!</h1>']
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [body.encode('utf-8')]
