2b94e82d47a711b07e2d86de7d77f00e10720d82
李军 <lijunSI0413@sogou-inc.com>
2017-02-24 17:21:34 +0800

    blog and uikit

--- awesome-python3-webapp/www/handlers.py
+++ awesome-python3-webapp/www/handlers.py
@@ -8,8 +8,13 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = "test"
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs 
     }
---
+++ test_log.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    logging.info("info")
