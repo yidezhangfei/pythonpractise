adef4ce07f0fd2378122310ddf293146710a3aba
yidezhangfei <yidezhangfei@163.com>
2017-05-05 18:51:30 +0800

    add multiprocess_rm.py

---
+++ multiprocess_rm.py
@@ -0,0 +1,61 @@
+# python3
+# coding: utf-8
+
+import os
+import sys
+import multiprocessing
+import stat
+
+def recursion_rm(dir):
+    dirs_or_files = os.listdir(dir)
+    for dir_or_file in dirs_or_files:
+        abdir_or_file = os.path.join(dir, dir_or_file)
+        if os.path.isdir(abdir_or_file):
+            recursion_rm(abdir_or_file)
+        elif os.path.isfile(abdir_or_file):
+            try:
+                if stat.S_IWRITE & os.stat(abdir_or_file).st_mode == 0:
+                    print (" file cannot write")
+                    os.chmod(abdir_or_file, stat.S_IWRITE)
+                os.remove(abdir_or_file)
+            except Exception as e:
+                raise e
+    try:
+        if stat.S_IWRITE & os.stat(dir).st_mode == 0:
+            print (" cannot write") 
+            s.chmod(dir, stat.S_IWRIT)
+        os.rmdir(dir)
+    except IOError as e:
+        raise e
+    return
+
+def recursion_rm_mp(dir):
+    if os.path.isdir(dir) is False:
+        raise "not a directory"
+    dirs_or_files = os.listdir(dir)
+    pool = multiprocessing.Pool(5)
+    for dir_or_file in dirs_or_files:
+        abdir_or_file = os.path.join(dir, dir_or_file)
+        if os.path.isdir(abdir_or_file):
+            pool.apply_async(recursion_rm, (), dict(dir=abdir_or_file))
+        elif os.path.isfile(abdir_or_file):
+            try:
+                os.remove(abdir_or_file)
+            except IOError as e:
+                raise e
+    pool.close()
+    pool.join()
+    try:
+        if stat.S_IWRITE & os.stat(dir).st_mode == 0:
+            os.chmod(dir, stat.S_IWRITE)
+        os.rmdir(dir)
+    except IOError as e:
+        raise e
+    return
+
+if __name__ == "__main__":
+    current_dir = os.getcwd()
+    dir = current_dir
+    if len(sys.argv) >= 2:
+        dir = sys.argv[1]
+    recursion_rm_mp(dir)
